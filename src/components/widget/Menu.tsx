d3b1d1207cb9de9dd948f8860273fdeaf8ba3f1a
Hamid Shamsian <hamid64bits@gmail.com>
2024-05-03 15:02:50 +0330

    feat: installed MUI v5 and drawer Menu added for navigation between pages.

--- src/components/widget/Menu.tsx
+++ src/components/widget/Menu.tsx
@@ -1,11 +1,152 @@
-import { ReactNode } from "react";
+import { useState, ReactNode } from "react";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+import CustomNavLink from "../common/CustomNavLink";
 
-interface MenuProps {
+const drawerWidth = 230;
+
+const primaryMenuItems = [
+  {
+    title: "News Feed",
+    to: "/",
+    icon: <NewspaperIcon />
+  },
+  {
+    title: "Search in News",
+    to: "search",
+    icon: <ManageSearchIcon />
+  }
+];
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen
+  }),
+  overflowX: "hidden"
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`
+  }
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-start",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== "open"
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== "open" })(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && { ...openedMixin(theme), "& .MuiDrawer-paper": openedMixin(theme) }),
+  ...(!open && { ...closedMixin(theme), "& .MuiDrawer-paper": closedMixin(theme) })
+}));
+
+interface AdminMenuProps {
   children: ReactNode;
 }
 
-const Menu = ({ children }: MenuProps) => {
-  return <div>{children}</div>;
+const Menu = ({ children }: AdminMenuProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar position='fixed' open={open}>
+        <Toolbar sx={{ display: "flex", justifyContent: open ? "flex-end" : "space-between" }}>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{ marginRight: 0, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap component='h1'>
+            News Aggregator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer variant='permanent' open={open} anchor='left'>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+
+        <List component='nav'>
+          {primaryMenuItems.map((item, i) => (
+            <ListItem key={i} disablePadding sx={{ display: "block", color: "inherit" }} component={CustomNavLink} to={item.to} end>
+              <ListItemButton sx={{ minHeight: 48, justifyContent: open ? "initial" : "center", px: 2.5 }}>
+                <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : "auto", justifyContent: "center" }}>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} sx={{ opacity: open ? 1 : 0, textAlign: "left" }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        {children}
+      </Box>
+    </Box>
+  );
 };
 
 export default Menu;
